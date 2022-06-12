46d86ba4c7b5b6225bcdb2c51a944d745f45eeb8
jamessawyer <550961784@qq.com>
2022-06-12 21:39:31 +0800

    feat: first cli with npm link

---
+++ 01-logname-cli/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+  luka
+
+  I'm following the setup instructions for this mobile javascript debugger here. FYI, I'm using Windows XP SP2 32-bit. Please understand I am new to using git, node.js, and npm.
+  Now, I'm stuck on this step
+
+  🐦 Twitter: https://www.twitter.com/jamessawyer
+  📖 Github: https://www.github.com/jamessawyer
+  😎 Blog: https://jamessawyer.com
+`)
