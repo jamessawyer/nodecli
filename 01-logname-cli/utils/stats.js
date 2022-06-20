d51ca04b277ab09f9ea1c371412f3eb9a352d54a
jamessawyer <550961784@qq.com>
2022-06-21 00:02:46 +0800

    feat: 使用 ora 显示loading animations

--- 01-logname-cli/utils/stats.js
+++ 01-logname-cli/utils/stats.js
@@ -1,14 +1,22 @@
 const axios = require('axios')
 const handleError = require('cli-handle-error')
 const to = require('await-to-js').default
+const ora = require('ora')
+const { green, yellow } = require('chalk')
 
 const gh_api = 'https://api.github.com/users/jamessawyer'
 
+const spinner = ora({ text: '' })
+
 module.exports = async () => {
+  spinner.start(yellow('Followers') + ' Fetching...')
   const [err, res] = await to(axios.get(gh_api))
 
   // handleError(heading = `ERROR: `, err, displayError = true, exit = true)
   handleError('接口调用失败:', err, true, true)
+
+  // spinner.stop()
+  spinner.succeed(green('succeed'))
 
   console.log(`
     GitHub Followers: ${res.data.followers}
