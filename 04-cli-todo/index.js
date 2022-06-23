eb0ceabf9f13ffcefb16e05be631c737ea22c5e7
jamessawyer <550961784@qq.com>
2022-06-23 21:43:40 +0800

    feat: 使用 make-dir 创建目录和fs创建文件

--- 04-cli-todo/index.js
+++ 04-cli-todo/index.js
@@ -6,6 +6,9 @@
  *
  * @author james sawyer <https://github.com/jamessawyer>
  */
+const makeDir = require('make-dir')
+const fs = require('fs')
+const path = require('path')
 
 const init = require('./utils/init');
 const cli = require('./utils/cli');
@@ -15,9 +18,25 @@ const input = cli.input;
 const flags = cli.flags;
 const { clear, debug } = flags;
 
-(async () => {
+
+// 使用json存储数据
+const dbTodos = path.join(process.cwd(), '.todo/todos.json')
+
+module.exports = (async () => {
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
+
+	if (!fs.existsSync(dbTodos)) {
+		// 不存在目录就创建目录
+		await makeDir('.todo')
+
+		// 切换到 .todo 目录
+		process.chdir('.todo')
+
+		// 写入文件
+		fs.writeFileSync('todos.json', '{}')
+
+	}
 
 	debug && log(flags);
 })();
